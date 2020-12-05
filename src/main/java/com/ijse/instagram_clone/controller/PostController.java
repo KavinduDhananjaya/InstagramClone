c22bace484824f8f42f448d853f4b09a26310a8f
kavindudhananjaya <dhananajayaa1998@gmail.com>
2020-12-05 16:42:04 +0530

    add bucket

--- src/main/java/com/ijse/instagram_clone/controller/PostController.java
+++ src/main/java/com/ijse/instagram_clone/controller/PostController.java
@@ -4,10 +4,17 @@ package com.ijse.instagram_clone.controller;
 import com.ijse.instagram_clone.entity.Post;
 import com.ijse.instagram_clone.service.impl.PostServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 @RequestMapping("/post")
@@ -20,8 +27,38 @@ public class PostController {
 
 
     @PostMapping("/add")
-    public Post addPost(@RequestBody Post post) {
+    public Post addPost(@RequestParam("file") MultipartFile file,Post post) {
+
+
+        File uploadedFile = new File(file.getOriginalFilename());
+
+        try (OutputStream os = new FileOutputStream(uploadedFile)) {
+            os.write(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String bucketName = "com.kavindu.bucket";
+        String objectKey = "test_file";
+
+
+        Region region = Region.US_WEST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        PutObjectResponse response = s3.putObject(PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(objectKey)
+                        .build(),
+                software.amazon.awssdk.core.sync.
+                RequestBody.fromFile(uploadedFile)
+        );
+        System.out.println(response.eTag());
+
+
         return postService.addPost(post);
+
     }
 
 
